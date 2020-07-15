9fdbb9dedd17659e69d327de8d36779bb76b6dc8
Ramil <ramil.minyukov@yandex.ru>
2020-07-15 22:37:09 +0300

    Add export topics from yandex news page

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import YandexNewsTopic
+
+
+@admin.register(YandexNewsTopic)
+class YandexNewsTopicAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rss_url']
+    search_fields = ['name']
+    ordering = ['name']
