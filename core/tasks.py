5669912707be531b322055c74b935efa7440dee4
dariacherbaeva <dashacherbaeva@mail.ru>
2020-07-23 10:07:06 +0300

    checking text added, requirement.txt updated

--- core/tasks.py
+++ core/tasks.py
@@ -6,6 +6,9 @@ from core.models import YandexNewsTopic, YandexNewsItem, TriggerPhrase
 from core.site_parser.site_parser import SiteParser
 from core.yandex_data_export.news_item import NewsItemDownloader
 from news_trigger.celery import app
+import re
+from flashtext import KeywordProcessor
+import pymorphy2
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -50,15 +53,33 @@ def check_yandex_news_for_trigger_words():
     news = YandexNewsItem.unchecked.all()[:5]  # all() [:5] for testing
     # init site parser
     parser = SiteParser()
-    # get trigger words
+    keyword_processor = KeywordProcessor()
     trigger_phrase = TriggerPhrase.objects.all()
+    # cleaning trigger phrases from punctuation and adding them to keyword processor
+    for t in trigger_phrase:
+        keyword_processor.add_keyword(re.sub(r'\W+', ' ', t.name))
+    morph = pymorphy2.MorphAnalyzer()
 
     for article in news.iterator():
         # get raw article text
         raw_text = parser.get_article_text(article.link)
-
-        # todo: checking the text
-        # checking ...
+        # cleaning the text
+        clean_text = re.sub(r'\W+', ' ', raw_text)
+        clean_name = re.sub(r'\W+', ' ', article.name)
+        # normalizing the text
+        final_text = ''
+        final_name = ''
+        for word in clean_text.split():
+            final_text += morph.parse(word)[0].normal_form
+            final_text += ' '
+        for word in clean_name.split():
+            final_name =+ morph.parse(word)[0].normal_form
+            final_name += ' '
+        # checking the text and article name for trigger words
+        if keyword_processor.extract_keywords(final_text) | keyword_processor.extract_keywords(final_name):
+            # todo: if article has keywords
+        else:
+            # todo: if it doesn't
 
         # update article
         article.checked = True
