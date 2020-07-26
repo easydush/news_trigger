ce90b3d36c6d27d52dc7269a4a75366382b50087
dariacherbaeva <dashacherbaeva@mail.ru>
2020-07-26 23:31:17 +0300

    Tests for Analyser added, requirements updated

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+from core.models import TriggerPhrase
+
+from core.util.ArticleAnalyser import ArticleAnalyser
+
+
+class TestTextAnalysis(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.ARTICLE_ANALYSER = ArticleAnalyser()
+
+    def test_cleaning_text(self):
+        text = "Roses (are) red, violets are blue!"
+        self.assertEqual(self.ARTICLE_ANALYSER.clean_text(text), 'Roses are red violets are blue ')
+
+    def test_normalizing_text(self):
+        self.assertEqual(self.ARTICLE_ANALYSER.get_word_with_normal_form('Розы'), 'роза')
+        self.assertEqual(self.ARTICLE_ANALYSER.get_word_with_normal_form('Делают'), 'делать')
