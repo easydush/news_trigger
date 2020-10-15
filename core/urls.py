a73935c794a5ca3501608b97ebb567ebff271bbb
Ramil <ramil.minyukov@yandex.ru>
2020-10-15 22:02:08 +0300

    Add news page
    Signed-off-by: Ramil <ramil.minyukov@yandex.ru>

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from core.views import HomeView
+from core.views import HomeView, NewsView
 
 app_name = 'core'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('news/', NewsView.as_view(), name='news')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from core.models import TriggerNews
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
@@ -12,4 +14,18 @@ class HomeView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context_data = super().get_context_data(**kwargs)
         context_data['main_page'] = True
+        return context_data
+
+
+class NewsView(LoginRequiredMixin, ListView):
+    """
+    Main page with all main information
+    """
+    template_name = 'core/news.html'
+    model = TriggerNews
+    paginate_by = 4
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['news_page'] = True
         return context_data
